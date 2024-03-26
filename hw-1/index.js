1789b6e6da156ab5fa3c2bedd49680ff288badf7
Sasha Muxa <mihal4ik11@gmail.com>
2024-03-26 21:07:25 +0200

    hw-1change

--- hw-1/index.js
+++ hw-1/index.js
@@ -1,5 +1,7 @@
 const hello = alert("Hello");
 const messageName = prompt("What is your name?");
 const messageFrom = prompt("Where are you from?");
-const result = `Nice to meet you, ${messageName}! You are from ${messageFrom}!`;
+const result = alert(
+  `Nice to meet you, ${messageName}! You are from ${messageFrom}!`
+);
 console.log(result);
