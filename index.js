8c33dbeb76b1de2f77579c1a35cc271d2d00f9d5
Sasha Muxa <mihal4ik11@gmail.com>
2024-03-26 20:22:38 +0200

    hw-1

---
+++ index.js
@@ -0,0 +1,5 @@
+const hello = alert("Hello");
+const messageName = prompt("What is your name?");
+const messageFrom = prompt("Where are you from?");
+const result = `Nice to meet you, ${messageName}! You are from ${messageFrom}!`;
+console.log(result);
