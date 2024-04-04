89ac39933ca8c6974b92de7f8283ce69b6ec83e9
Sasha Muxa <mihal4ik11@gmail.com>
2024-04-04 07:44:19 +0300

    hw-2

--- hw-1/index.js
+++ hw-1/index.js
@@ -1,7 +1,4 @@
-const hello = alert("Hello");
+alert("Hello");
 const messageName = prompt("What is your name?");
 const messageFrom = prompt("Where are you from?");
-const result = alert(
-  `Nice to meet you, ${messageName}! You are from ${messageFrom}!`
-);
-console.log(result);
+alert(`Nice to meet you, ${messageName}! You are from ${messageFrom}!`);
---
+++ hw-2/index.js
@@ -0,0 +1,8 @@
+alert("Hello");
+const messageName = prompt("What is your name?");
+const messageAge = prompt("How old are you?");
+const messageFromCountry = prompt("Which country are you from");
+const messageFromCity = prompt("Which city are you from?");
+alert(
+  `Nice to meet you, ${messageName}! You are ${messageAge} years old You are from ${messageFromCity}! From the city of ${messageFromCity}`
+);
