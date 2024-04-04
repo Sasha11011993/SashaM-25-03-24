b185ba84464f2adb30ccf8b667ffca0e44560430
Sasha Muxa <mihal4ik11@gmail.com>
2024-04-04 08:14:41 +0300

    hw-3

---
+++ hw-3/index.js
@@ -0,0 +1,26 @@
+const myName = "Sasha";
+console.log(typeof myName);
+
+const myAge = 31;
+console.log(typeof myAge);
+
+const bigInt = 10n;
+console.log(typeof bigInt);
+
+const boolean = true;
+console.log(typeof boolean);
+
+const nuLL = null;
+console.log(typeof nuLL);
+
+const undeFined = undefined;
+console.log(typeof undeFined);
+
+const symbol = Symbol("foo");
+console.log(typeof symbol);
+
+const myNameMyAge = {
+  name: "Sasha",
+  age: 31,
+};
+console.log(typeof myNameMyAge);
