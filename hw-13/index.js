2a4b7f13165d1c397aaf355fd624de91b1e49a22
Sasha Muxa <mihal4ik11@gmail.com>
2024-04-13 08:01:16 +0300

    hw-13

---
+++ hw-13/index.js
@@ -0,0 +1,12 @@
+const string = prompt("Привіт, введи рядок");
+const symbol = prompt("Напиши які потрібно видалити букви");
+
+function deleteSymbol(string, symbol) {
+  symbol.split("").forEach(function (symbol) {
+    string = string.replaceAll(symbol, "");
+  });
+  return string;
+}
+
+const result = deleteSymbol(string, symbol);
+console.log(result);
