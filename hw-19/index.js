7d855ea2adcb5e9191457e2d6daf5098c62ba352
Sasha Muxa <mihal4ik11@gmail.com>
2024-04-21 08:14:58 +0300

    hw-19

---
+++ hw-19/index.js
@@ -0,0 +1,23 @@
+let ladder = {
+  step: 0,
+
+  up: function () {
+    // підніматиме вас на одну сходинку
+    this.step++;
+    return this;
+  },
+
+  down: function () {
+    // опускатиме вас на одну сходинку
+    this.step--;
+    return this;
+  },
+
+  showStep: function () {
+    // показує поточну сходинку
+    console.log(this.step);
+    return this;
+  },
+};
+
+ladder.up().up().down().showStep();
