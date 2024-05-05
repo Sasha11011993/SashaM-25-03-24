ade358eb4cec15da5630a13eaff9b4fad7124816
Sasha Muxa <mihal4ik11@gmail.com>
2024-05-05 16:58:17 +0300

    hw-ToDo

--- hw-23.3/index.js
+++ hw-23.3/index.js
@@ -1,23 +1,74 @@
-document.querySelector("#container").addEventListener("click", function (e) {
-  e.target.style.backgroundColor = "green";
+const list = document.querySelector("#todo-list");
+const input = document.querySelector("#todo-input-container input");
+const button = document.querySelector("#todo-input-container button");
+
+button.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.id = "todo-li";
+  const divCreate = document.createElement("div");
+  divCreate.id = "li-div";
+  const btnChange = document.createElement("button");
+  btnChange.id = "btnChange";
+  btnChange.textContent = "Change";
+  const btnDelete = document.createElement("button");
+  btnDelete.id = "btnDelete";
+  btnDelete.textContent = "Delete";
+  li.innerText = input.value;
+  list.appendChild(li);
+  list.appendChild(li);
+  li.appendChild(divCreate);
+  divCreate.appendChild(btnChange);
+  divCreate.appendChild(btnDelete);
+  input.value = "";
+
+  btnDelete.addEventListener("click", () => {
+    list.removeChild(li);
+  });
+
+  btnChange.addEventListener("click", () => {
+    const divChange = document.createElement("div");
+    divChange.id = "btnChange-div";
+    const changeInput = document.createElement("input");
+    changeInput.id = "changeInput";
+    changeInput.placeholder = "Введіть нове завдання";
+    const btnSave = document.createElement("button");
+    btnSave.id = "btnSave";
+    btnSave.textContent = "Save";
+    const btnNo = document.createElement("button");
+    btnNo.id = "btnNo";
+    btnNo.textContent = "No";
+
+    divChange.appendChild(changeInput);
+    divChange.appendChild(btnSave);
+    divChange.appendChild(btnNo);
+    li.appendChild(divChange);
+    li.removeChild(divCreate);
+
+    btnSave.addEventListener("click", () => {
+      li.innerText = changeInput.value;
+      li.appendChild(divCreate);
+    });
+
+    btnNo.addEventListener("click", () => {
+      li.removeChild(divChange);
+      li.appendChild(divCreate);
+    });
+  });
 });
 
-// button[i].addEventListener("mouseout", function (e) {
-//   e.target.style.background = "none";
-// });
+// button.addEventListener("click", () => {
+//   const li = document.createElement("li");
+//   li.id = "todo-li";
+//   const btnDelete = document.createElement("button");
+//   btnDelete.id = "btnDelete";
+//   btnDelete.textContent = "Delete";
+//   li.innerText = input.value;
+//   list.appendChild(li);
+//   li.appendChild(btnDelete);
 
-// buttons.forEach((button) => {
-//   button.addEventListener("abort", function (e) {
-//     e.target.style.background = "green";
-//   });
-// });
+//   input.value = "";
 
-// buttons.forEach((button) => {
-//   button.addEventListener("mouseover", function (e) {
-//     e.target.style.background = "green";
-//   });
-
-//   button.addEventListener("mouseout", function (e) {
-//     e.target.style.background = "none";
+//   btnDelete.addEventListener("click", () => {
+//     list.removeChild(li);
 //   });
 // });
