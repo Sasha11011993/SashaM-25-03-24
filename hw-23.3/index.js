af9e13177c9bcb34d4164b0b6ec1cc8d32a76ec2
Sasha Muxa <mihal4ik11@gmail.com>
2024-05-06 17:34:18 +0300

    ToDo

--- hw-23.3/index.js
+++ hw-23.3/index.js
@@ -5,70 +5,77 @@ const button = document.querySelector("#todo-input-container button");
 button.addEventListener("click", () => {
   const li = document.createElement("li");
   li.id = "todo-li";
-  const divCreate = document.createElement("div");
-  divCreate.id = "li-div";
-  const btnChange = document.createElement("button");
-  btnChange.id = "btnChange";
-  btnChange.textContent = "Change";
+
+  const p = document.createElement("p");
+  p.id = "par";
+
   const btnDelete = document.createElement("button");
   btnDelete.id = "btnDelete";
   btnDelete.textContent = "Delete";
-  li.innerText = input.value;
+
+  p.innerText = input.value;
+
   list.appendChild(li);
-  list.appendChild(li);
-  li.appendChild(divCreate);
-  divCreate.appendChild(btnChange);
-  divCreate.appendChild(btnDelete);
+  li.appendChild(p);
+  li.appendChild(btnDelete);
+
   input.value = "";
 
   btnDelete.addEventListener("click", () => {
     list.removeChild(li);
-  });
-
-  btnChange.addEventListener("click", () => {
-    const divChange = document.createElement("div");
-    divChange.id = "btnChange-div";
-    const changeInput = document.createElement("input");
-    changeInput.id = "changeInput";
-    changeInput.placeholder = "Введіть нове завдання";
-    const btnSave = document.createElement("button");
-    btnSave.id = "btnSave";
-    btnSave.textContent = "Save";
-    const btnNo = document.createElement("button");
-    btnNo.id = "btnNo";
-    btnNo.textContent = "No";
-
-    divChange.appendChild(changeInput);
-    divChange.appendChild(btnSave);
-    divChange.appendChild(btnNo);
-    li.appendChild(divChange);
-    li.removeChild(divCreate);
-
-    btnSave.addEventListener("click", () => {
-      li.innerText = changeInput.value;
-      li.appendChild(divCreate);
-    });
-
-    btnNo.addEventListener("click", () => {
-      li.removeChild(divChange);
-      li.appendChild(divCreate);
-    });
   });
 });
 
 // button.addEventListener("click", () => {
 //   const li = document.createElement("li");
 //   li.id = "todo-li";
+//   const divCreate = document.createElement("div");
+//   divCreate.id = "li-div";
+//   const btnChange = document.createElement("button");
+//   btnChange.id = "btnChange";
+//   btnChange.textContent = "Change";
 //   const btnDelete = document.createElement("button");
 //   btnDelete.id = "btnDelete";
 //   btnDelete.textContent = "Delete";
-//   li.innerText = input.value;
-//   list.appendChild(li);
-//   li.appendChild(btnDelete);
+//   li.textContent = input.value;
+//   list.appendChild(li);//
+//   li.appendChild(divCreate);
 
+//   divCreate.appendChild(btnChange);
+//   divCreate.appendChild(btnDelete);
 //   input.value = "";
 
 //   btnDelete.addEventListener("click", () => {
 //     list.removeChild(li);
+//   });
+
+//   btnChange.addEventListener("click", () => {
+//     const divChange = document.createElement("div");
+//     divChange.id = "btnChange-div";
+//     const changeInput = document.createElement("input");
+//     changeInput.id = "changeInput";
+//     changeInput.placeholder = "Введіть нове завдання";
+//     const btnSave = document.createElement("button");
+//     btnSave.id = "btnSave";
+//     btnSave.textContent = "Save";
+//     const btnNo = document.createElement("button");
+//     btnNo.id = "btnNo";
+//     btnNo.textContent = "No";
+
+//     divChange.appendChild(changeInput);
+//     divChange.appendChild(btnSave);
+//     divChange.appendChild(btnNo);
+//     li.appendChild(divChange);
+//     li.removeChild(divCreate);
+
+//     btnSave.addEventListener("click", () => {
+//       li.innerText = changeInput.value;
+//       li.appendChild(divCreate);
+//     });
+
+//     btnNo.addEventListener("click", () => {
+//       li.removeChild(divChange);
+//       li.appendChild(divCreate);
+//     });
 //   });
 // });
